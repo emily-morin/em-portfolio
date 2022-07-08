db9ec65fbba626782c9dfdb34c08c21349d482ee
emily_m <morin.emily@gmail.com>
2022-07-08 10:31:11 -0400

    Add functionality to back to top button

--- js/top-btn.js
+++ js/top-btn.js
@@ -1,3 +1,17 @@
-function scrollTop () {
-    
+// get button
+const topButton = document.getElementById("top-button");
+
+// display button after user scrolls down 20px from top of page
+function showButton () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block"; 
+    } else {
+        topButton.style.display = "none"; 
+    }
+}
+
+// bring user back to top of page when button is clicked
+function scrollToTop () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
