f68e124b625e2fffef58fde954d5def9cdee3bd4
emily_m <morin.emily@gmail.com>
2022-07-18 16:33:40 -0400

    Add tablet spacings in footer and skills section

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// auto-update date in the footer copyright line 
+// const year = document.getElementById("year");
+// let currentYear = new Date().getFullYear();
+// window.onload = function () {
+//     year.innerHTML = currentYear;
+// }
+
+const date = document.getElementById("date");
+  date.innerHTML = new Date().getFullYear();
