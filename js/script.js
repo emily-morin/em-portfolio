ee04e54711d44e39ef23134f668648e62a6a21a8
emily_m <morin.emily@gmail.com>
2022-08-03 14:48:21 -0400

    Edit about section content, add bottom borders for projects in mobile view, clean up comments in code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 // auto-update date in the footer copyright line 
-// const year = document.getElementById("year");
-// let currentYear = new Date().getFullYear();
-// window.onload = function () {
-//     year.innerHTML = currentYear;
-// }
 
 const date = document.getElementById("date");
   date.innerHTML = new Date().getFullYear();
